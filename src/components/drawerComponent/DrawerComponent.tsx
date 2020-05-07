d643dc14721322edaaca21b5d051b708abbb83c9
mallorax <patryk733773@gmail.com>
2020-05-07 20:44:52 +0200

    Added buttons to drawer

--- src/components/drawerComponent/DrawerComponent.tsx
+++ src/components/drawerComponent/DrawerComponent.tsx
@@ -6,7 +6,9 @@ import { useHistory } from 'react-router';
 const makeClasses = makeStyles((theme: Theme) => ({
     drawerContent: {
         margin: '20px',
-    }
+        display: 'flex',
+        flexDirection: 'column',
+    },
 }));
 
 interface IDrawerComponentProps {
@@ -18,7 +20,15 @@ const DrawerComponent: React.FC<IDrawerComponentProps> = ({ shouldBeOpen }) => {
     const [isOpen, setIsOpen] = React.useState(false);
     const history = useHistory();
 
-    const RedirectTo = (path: string, name: string) => <div onClick={() => history.push(path)}>{name}</div>
+	interface RedirectToProps {
+		path: string
+		name: string
+	}
+	// FUNCTION
+ 	//const RedirectTo = (path: string, name: string) => <div onClick={() => history.push(path)}>{name}</div>
+
+	// FUNCTIONAL COMPONENT
+    const RedirectTo = ({path, name}: RedirectToProps) => <button onClick={() => history.push(path)}>{name}</button>
 
     return (
         <div>
@@ -27,11 +37,9 @@ const DrawerComponent: React.FC<IDrawerComponentProps> = ({ shouldBeOpen }) => {
                 onClose={() => setIsOpen(false)}
             >
                 <div className={classes.drawerContent}>
-                    <ul>
-                        <li>{RedirectTo('/', 'Home')}</li>
-                        <li>{RedirectTo('/movie', 'Movie')}</li>
-                        <li>{RedirectTo('/search', 'Search Movie')}</li>
-                    </ul>
+                        <RedirectTo path="/" name="Home"/>
+                        <RedirectTo path="/movie" name="Home"/>
+                        <RedirectTo path="/search" name="Search Movie"/>
                 </div>
             </Drawer>
         </div>
